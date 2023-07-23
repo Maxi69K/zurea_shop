d45e8334955c08cc7e6eba02d1740a3b137d4b37
Goran Kekez <gorankekez@gmail.com>
2023-07-23 12:53:36 +0200

    add random product func

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -8,4 +8,32 @@ productRoute.get('/get-all', (req, res) => {
     .catch(err => res.status(420).send('Error in DB.'));
 });
 
+// Random Product
+productRoute.get('/random/:number', async (req, res)  => {
+  let count = await ProductModel.count({});
+
+  //let randomProduct = Math.floor(Math.random() * count + 1);
+
+  let query = {
+    skip: 0, // Skips first enter a number (products in base)
+    limit: parseInt(req.params.number), // Determines how many products will be displayed
+    sort: { price: 1 }, // to sort the price with the highest value use sort: { price: -1 };
+  };
+
+  // To display all products use projection = null;
+  // throw out some value and use it projection = { price: 0 }
+  // To display certain values projection = { price: 1, title: 1, description: 1, rating: 0 } itc...
+  let projection = null;
+
+  ProductModel.find({}, projection, query)
+    .then((data) => {
+      //console.log(data);
+      res.send('ok');
+    })
+    .catch((err) => {
+      //console.log(err);
+      res.status(420).send('Error in DB.');
+    });
+})
+
 module.exports = productRoute;

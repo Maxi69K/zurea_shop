6a5b358918af49a26ae2063e5569cd0fd4b8c9e8
Goran Kekez <gorankekez@gmail.com>
2023-08-24 10:22:51 +0200

    fixed cookie

--- frontend/src/components/CookieNotice/CookieNotice.Component.jsx
+++ frontend/src/components/CookieNotice/CookieNotice.Component.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import {getCookie, setCookie} from 'tiny-cookie';
 
+// for cookies
 const CookieNoticeComponent = () => {
     const [items, setItems] = useState({
         name: 'zureaCookie',
